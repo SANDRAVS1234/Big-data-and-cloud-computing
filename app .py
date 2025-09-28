cc1decac4fdd5ff36d4f58268a072190a8e77548
SANDRAVS1234 <sandra.ds24@duk.ac.in>
2025-09-28 13:30:50 +0530

    Update and rename app (1).py to app .py

--- app .py
+++ app .py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1uobwOq6FgAhU5uRlCGhMbnSxcgeWuDwV
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import requests
@@ -21,6 +19,7 @@ from pyspark.ml.feature import Tokenizer, HashingTF, IDF
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 
 # Setup
 # ---------------------------
